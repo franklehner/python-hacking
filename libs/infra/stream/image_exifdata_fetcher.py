af2e08f175ea616d825a0d8a21ffe8bd828eea54
Frank Lehner <frank.lehner@online.de>
2020-03-16 11:29:25 +0100

    image_exifdata_fetcher: Initial commit

---
+++ libs/infra/stream/image_exifdata_fetcher.py
@@ -0,0 +1,15 @@
+"""
+image_exifdata_fetcher
+"""
+
+
+# import os as _os
+
+import libs.domain.entities.image_exifdata_fetcher as _entity
+
+
+class ExifRepository(_entity.ExifData):
+    """
+    ExifRepository
+    """
+    pass
